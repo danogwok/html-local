873439b55d49a26be3c8f732f25e8b8b21c31b53
therisingpage <devynprincess@gmail.com>
2013-04-19 14:55:06 +0300

    Deleted question marks

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <head>
 <meta charset="utf-8">
 <title>TerrAfrica</title>
-<?php
+<php
 
   $bg = array('bg-01.jpg', 'bg-02.jpg', 'bg-03.jpg', 'bg-04.jpg' ); // array of filenames
 
   $i = rand(0, count($bg)-1); // generate random number size of the array
   $selectedBg = "$bg[$i]"; // set variable equal to which random filename was chosen
-?>
+>
 <style type="text/css">
 * {
 	margin: 0 auto;
